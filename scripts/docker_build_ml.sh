0c75c13883da1510b8092621a444a1e6005df10f
yarikgoldvarg <40838787+yarikgoldvarg@users.noreply.github.com>
2020-08-05 14:42:00 +0300

    Update docker_build_ml.sh

--- scripts/docker_build_ml.sh
+++ scripts/docker_build_ml.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.4.3"
+
 #
 # Machine Learning + jetson-inference
 #
-sh ./scripts/docker_build.sh l4t-ml:r32.4.3-py3 Dockerfile.ml \
+sh ./scripts/docker_build.sh jetson-inference:latest Dockerfile.ml \
 		--build-arg BASE_IMAGE=$BASE_IMAGE \
 		--build-arg PYTORCH_IMAGE=l4t-pytorch:r32.4.3-pth1.6-py3 \
 		--build-arg TENSORFLOW_IMAGE=l4t-tensorflow:r32.4.3-tf1.15-py3
