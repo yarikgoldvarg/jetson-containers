c5736ca9e32471cd093a61f2f93f1f90151b3862
yarikgoldvarg <40838787+yarikgoldvarg@users.noreply.github.com>
2020-08-05 14:44:20 +0300

    Update docker_build_ml.sh

--- scripts/docker_build_ml.sh
+++ scripts/docker_build_ml.sh
@@ -5,6 +5,90 @@ set -e
 BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.4.3"
 
 #
+# PyTorch (for JetPack 4.4)
+#
+#  PyTorch v1.2.0 https://nvidia.box.com/shared/static/lufbgr3xu2uha40cs9ryq1zn4kxsnogl.whl (torch-1.2.0-cp36-cp36m-linux_aarch64.whl)
+#  PyTorch v1.3.0 https://nvidia.box.com/shared/static/017sci9z4a0xhtwrb4ps52frdfti9iw0.whl (torch-1.3.0-cp36-cp36m-linux_aarch64.whl)
+#  PyTorch v1.4.0 https://nvidia.box.com/shared/static/c3d7vm4gcs9m728j6o5vjay2jdedqb55.whl (torch-1.4.0-cp36-cp36m-linux_aarch64.whl)
+#  PyTorch v1.5.0 https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl (torch-1.5.0-cp36-cp36m-linux_aarch64.whl)
+#
+build_pytorch()
+{
+	local pytorch_url=$1
+	local pytorch_whl=$2
+	local pytorch_tag=$3
+	
+	local vision_version=$4
+	local pillow_version=$5
+	local audio_version=$6
+
+	echo "building PyTorch $pytorch_whl, torchvision $vision_version ($pillow_version), torchaudio $audio_version"
+
+	sh ./scripts/docker_build.sh $pytorch_tag Dockerfile.pytorch \
+			--build-arg BASE_IMAGE=$BASE_IMAGE \
+			--build-arg PYTORCH_URL=$pytorch_url \
+			--build-arg PYTORCH_WHL=$pytorch_whl \
+			--build-arg TORCHVISION_VERSION=$vision_version \
+			--build-arg PILLOW_VERSION=$pillow_version \
+			--build-arg TORCHAUDIO_VERSION=$audio_version
+
+	echo "done building PyTorch $pytorch_whl, torchvision $vision_version ($pillow_version), torchaudio $audio_version"
+}
+
+# PyTorch v1.2.0
+#build_pytorch "https://nvidia.box.com/shared/static/lufbgr3xu2uha40cs9ryq1zn4kxsnogl.whl" \
+#			  "torch-1.2.0-cp36-cp36m-linux_aarch64.whl" \
+#			  "l4t-pytorch:r32.4.3-pth1.2-py3" \
+#			  "v0.4.0" \
+#			  "pillow<7"
+
+# PyTorch v1.3.0
+#build_pytorch "https://nvidia.box.com/shared/static/017sci9z4a0xhtwrb4ps52frdfti9iw0.whl" \
+#			  "torch-1.3.0-cp36-cp36m-linux_aarch64.whl" \
+#			  "l4t-pytorch:r32.4.3-pth1.3-py3" \
+#			  "v0.4.2" \
+#			  "pillow<7"  
+
+# PyTorch v1.4.0
+#build_pytorch "https://nvidia.box.com/shared/static/c3d7vm4gcs9m728j6o5vjay2jdedqb55.whl" \
+#			  "torch-1.4.0-cp36-cp36m-linux_aarch64.whl" \
+#			  "l4t-pytorch:r32.4.3-pth1.4-py3" \
+#			  "v0.5.0" \
+#			  "pillow" 
+
+# PyTorch v1.5.0
+#build_pytorch "https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl" \
+#			  "torch-1.5.0-cp36-cp36m-linux_aarch64.whl" \
+#			  "l4t-pytorch:r32.4.3-pth1.5-py3" \
+#			  "v0.6.0" \
+#			  "pillow" 
+
+
+
+#			  
+# TensorFlow (for JetPack 4.4)
+#
+#  TensorFlow 1.15.2 https://nvidia.box.com/shared/static/8a3q3dz6juk0xg2e2kwwng9teosyohad.whl (tensorflow-1.15.2+nv20.6-cp36-cp36m-linux_aarch64.whl)
+#  TensorFlow 2.2.0  https://nvidia.box.com/shared/static/l5lzgqh6cm5kw1b1nzdzuwcpf70xndak.whl (tensorflow-2.2.0+nv20.6-cp36-cp36m-linux_aarch64.whl)
+#
+build_tensorflow()
+{
+	local tensorflow_url=$1
+	local tensorflow_whl=$2
+	local tensorflow_tag=$3
+	
+	echo "building TensorFlow $tensorflow_whl, $tensorflow_tag"
+
+	sh ./scripts/docker_build.sh $tensorflow_tag Dockerfile.tensorflow \
+		--build-arg BASE_IMAGE=$BASE_IMAGE \
+		--build-arg TENSORFLOW_URL=$tensorflow_url \
+		--build-arg TENSORFLOW_WHL=$tensorflow_whl
+
+	echo "done building TensorFlow $tensorflow_whl, $tensorflow_tag"
+}
+
+
+#
 # Machine Learning + jetson-inference
 #
 sh ./scripts/docker_build.sh jetson-inference:latest Dockerfile.ml \
